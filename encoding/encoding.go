483a6f2ea3ffb6ae66172455f7480f0bf839f2b7
Dmitry Fedotov <FedotovQA@yandex.ru>
2024-08-11 15:23:48 +0300

    encoding complete

--- encoding/encoding.go
+++ encoding/encoding.go
@@ -1,7 +1,12 @@
 package encoding
 
 import (
+	"encoding/json"
+	"io"
+	"os"
+
 	"github.com/Yandex-Practicum/final-project-encoding-go/models"
+	"gopkg.in/yaml.v3"
 )
 
 // JSONData тип для перекодирования из JSON в YAML
@@ -26,15 +31,63 @@ type MyEncoder interface {
 // Encoding перекодирует файл из JSON в YAML
 func (j *JSONData) Encoding() error {
 	// ниже реализуйте метод
-	// ...
-
+	var obj interface{}
+	JSONdata, err := os.Open("jsonInput.json")
+	if err != nil {
+		return err
+	}
+	data, err := io.ReadAll(JSONdata)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(data, &obj)
+	if err != nil {
+		return err
+	}
+	out, err := yaml.Marshal(obj)
+	if err != nil {
+		return err
+	}
+	yamlFile, err := os.Create("yamlOutput.yml")
+	if err != nil {
+		return err
+	}
+	defer yamlFile.Close()
+	_, err = yamlFile.Write(out)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 // Encoding перекодирует файл из YAML в JSON
 func (y *YAMLData) Encoding() error {
 	// Ниже реализуйте метод
-	// ...
-
+	var obj interface{}
+	YAMLdata, err := os.Open("yamlInput.yml")
+	if err != nil {
+		return err
+	}
+	data, err := io.ReadAll(YAMLdata)
+	if err != nil {
+		return err
+	}
+	err = yaml.Unmarshal(data, &obj)
+	if err != nil {
+		return err
+	}
+	out, err := json.Marshal(obj)
+	if err != nil {
+		return err
+	}
+	JSONFile, err := os.Create("jsonOutput.json")
+	if err != nil {
+		return err
+	}
+	defer JSONFile.Close()
+	_, err = JSONFile.Write(out)
+	if err != nil {
+		return err
+	}
 	return nil
 }
